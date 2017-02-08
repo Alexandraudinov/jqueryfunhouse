fcca0a8cee1de47acaed30ac69f1a6313a2947bf
Alexandra Udinov <rosanique.l.hall@student.uncommonhs.org>
2017-02-08 22:26:05 +0000

    I chaged Colors

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
 $(document).ready( function(){
-    $('#change-color').click(function(){
-        $('body').css("background-color","red");
-    })
-})
+    $("#change-color").click(function(){
+        var colors= ["red", "blue", "green", "yellow", "black"]
+        $('body').css("background-color", colors[Math.round(Math.random() *4)]);
+         })
+    $("#submit-name").click(function(){
+    $("#greeting").html("Welcome and Congratulations, " + $("#name").val());
+    })  
+})    
+
+
 
